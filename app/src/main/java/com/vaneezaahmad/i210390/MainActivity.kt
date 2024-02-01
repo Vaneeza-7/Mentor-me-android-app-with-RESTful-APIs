58998f40382b9e86532c07bf7a36be4c3562b129
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-02-01 23:58:17 +0500

    first screen

---
+++ app/src/main/java/com/vaneezaahmad/i210390/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.vaneezaahmad.i210390
+
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+/*        val value : ImageView = findViewById (R.id.logo)
+
+        value.setBackgroundColor(resources.getColor(android.R.color.transparent))*/
+
+    }
+}
