70bf6b7c3ffd0da475660c9eee1522ea26a1f57a
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-02-02 01:38:30 +0500

    second screen

---
+++ app/src/main/java/com/vaneezaahmad/i210390/Activity2.kt
@@ -0,0 +1,11 @@
+package com.vaneezaahmad.i210390
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
--- app/src/main/java/com/vaneezaahmad/i210390/MainActivity.kt
+++ app/src/main/java/com/vaneezaahmad/i210390/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.vaneezaahmad.i210390
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.ImageView
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-/*        val value : ImageView = findViewById (R.id.logo)
+        /*        val value : ImageView = findViewById (R.id.logo)
 
         value.setBackgroundColor(resources.getColor(android.R.color.transparent))*/
 
+        var btn = findViewById<TextView>(R.id.textView1)
+        btn.setOnClickListener {
+            startActivity(
+                Intent(this, Activity2::class.java)
+            );
+
+        }
     }
 }
