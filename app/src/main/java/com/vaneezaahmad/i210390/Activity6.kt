d9003ea418ce7e0442161bc5382dca89b556b2d3
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-02-04 02:28:08 +0500

    sixth screen & rounded fields in 1st screen

--- app/src/main/java/com/vaneezaahmad/i210390/Activity6.kt
+++ app/src/main/java/com/vaneezaahmad/i210390/Activity6.kt
@@ -1,27 +1,21 @@
 package com.vaneezaahmad.i210390
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageView
+import android.content.Intent
+import android.view.View
 
-class Activity5 : AppCompatActivity() {
+class Activity6 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_5)
+        setContentView(R.layout.activity_6)
+
 
         var back = findViewById<ImageView>(R.id.arrow)
         back.setOnClickListener {
             startActivity(
-                Intent(this, Activity2::class.java)
-            );
-        }
-
-        var next = findViewById<View>(R.id.submit)
-        next.setOnClickListener {
-            startActivity(
-                Intent(this, Activity6::class.java)
+                Intent(this, Activity5::class.java)
             );
         }
 
@@ -31,6 +25,6 @@ class Activity5 : AppCompatActivity() {
                 Intent(this, Activity2::class.java)
             );
         }
-
     }
+
 }
