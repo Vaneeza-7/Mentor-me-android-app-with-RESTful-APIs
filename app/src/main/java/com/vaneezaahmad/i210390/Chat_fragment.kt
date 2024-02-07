e436242b1f991efa8893704845168a015706c095
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-02-08 00:35:05 +0500

    chat fragment complete, screen 14

--- app/src/main/java/com/vaneezaahmad/i210390/Chat_fragment.kt
+++ app/src/main/java/com/vaneezaahmad/i210390/Chat_fragment.kt
@@ -1,6 +1,24 @@
 package com.vaneezaahmad.i210390
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class Chat_fragment : Fragment(R.layout.fragment_chat){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val back = view.findViewById<ImageView>(R.id.backButton)
+
+        back.setOnClickListener {
+            // Replace the current fragment with the new fragment
+            val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container_view, Home_fragment())
+            fragmentTransaction.addToBackStack(null) // Optional: Add transaction to back stack
+            fragmentTransaction.commit()
+        }
+    }
 }
