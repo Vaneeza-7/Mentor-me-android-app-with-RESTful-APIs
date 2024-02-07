ec3f2b70e4dfd32cb817138844b1bef4632ba195
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-02-08 01:51:31 +0500

    notifications screen 24 complete,

--- app/src/main/java/com/vaneezaahmad/i210390/Home_fragment.kt
+++ app/src/main/java/com/vaneezaahmad/i210390/Home_fragment.kt
@@ -1,9 +1,11 @@
 package com.vaneezaahmad.i210390
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.ScrollView
 import androidx.fragment.app.Fragment
 
@@ -14,6 +16,17 @@ class Home_fragment : Fragment(R.layout.fragment_home){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageView>(R.id.bell).setOnClickListener {
+            // Create an Intent to start the target activity
+            val intent = Intent(requireContext(), NotificationsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/vaneezaahmad/i210390/NotificationsActivity.kt
@@ -0,0 +1,20 @@
+package com.vaneezaahmad.i210390
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class NotificationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifications)
+
+        val back = findViewById<ImageView>(R.id.arrow)
+        back.setOnClickListener {
+            startActivity(
+                Intent(this, Activity7::class.java)
+            );
+        }
+    }
+}
