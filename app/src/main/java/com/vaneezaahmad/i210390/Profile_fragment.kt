8c8964589889074d2d7c9add762dc35e90b2fcb5
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-02-12 03:15:51 +0500

    screen 21, Profile fragment complete

--- app/src/main/java/com/vaneezaahmad/i210390/Profile_fragment.kt
+++ app/src/main/java/com/vaneezaahmad/i210390/Profile_fragment.kt
@@ -1,6 +1,48 @@
 package com.vaneezaahmad.i210390
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 
 class Profile_fragment : Fragment(R.layout.fragment_profile){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<CardView>(R.id.card).setOnClickListener {
+            val intent = Intent(requireContext(), Activity8::class.java)
+            startActivity(intent)
+        }
+
+
+        view.findViewById<ImageView>(R.id.backButton).setOnClickListener {
+            val intent = Intent(requireContext(), Activity7::class.java)
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.iconButton).setOnClickListener {
+            val intent = Intent(requireContext(), Activity12::class.java)
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.iconButton2).setOnClickListener {
+            val intent = Intent(requireContext(), Activity12::class.java)
+            startActivity(intent)
+        }
+
+    }
 }
