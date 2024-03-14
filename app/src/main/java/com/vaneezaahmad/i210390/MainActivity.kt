c631a9c48ce710c35412cd38a660e3572696435d
Vaneeza-7 <vaneezaahmad786@gmail.com>
2024-03-14 08:28:01 +0500

    create user, and saved user data to realtime db + auto splash screen

--- app/src/main/java/com/vaneezaahmad/i210390/MainActivity.kt
+++ app/src/main/java/com/vaneezaahmad/i210390/MainActivity.kt
@@ -3,7 +3,9 @@ package com.vaneezaahmad.i210390
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.widget.TextView
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +15,16 @@ class MainActivity : AppCompatActivity() {
 
         value.setBackgroundColor(resources.getColor(android.R.color.transparent))*/
 
-        var btn = findViewById<TextView>(R.id.textView1)
+        /*var btn = findViewById<TextView>(R.id.textView1)
         btn.setOnClickListener {
             startActivity(
                 Intent(this, Activity2::class.java)
             );
 
-        }
+        }*/
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, Activity2::class.java))
+            finish()
+        }, 5000) // 5000 milliseconds = 5 seconds
     }
 }
